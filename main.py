eefc04cda0b5a29cfb84d8fee3022cb1755eba6f
surendra d <surendra.dudi@otocapital.in>
2022-07-21 15:40:47 +0530

    added the files to hello_app

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Welcome To This Site, it's a web app!"
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8083, debug=True)
